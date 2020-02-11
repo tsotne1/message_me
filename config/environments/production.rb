83c38f7ccb028663e6d477ab6306815b14e7eed2
tsotne1 <tsotne.mazmishvili.1@btu.edu.ge>
2020-02-11 17:33:00 +0400

    final commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://https://messageme-tsotne.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://messageme-tsotne.herokuapp.com/', 'http://messageme-tsotne.herokuapp.com/']
 
 end
