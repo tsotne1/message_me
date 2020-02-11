bf2dfae50a2e1552ead9fd5a7db6654f67a7d915
tsotne1 <tsotne.mazmishvili.1@btu.edu.ge>
2020-02-11 17:14:56 +0400

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
-
+  post 'signup', to: 'users#create'
+  get 'signup', to: 'users#new'
+  
   mount ActionCable.server, at: '/cable'
 end
